981369a927fe98160df165ed72bcc2e4a5cf8356
Hari Kuchipudi <harikuchipudi1@gmail.com>
2024-10-18 11:16:08 +0530

    backend initial commit

---
+++ DataStructuresBackend/src/main/java/com/example/DataStructuresBackend/DataStructuresBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataStructuresBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStructuresBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStructuresBackendApplication.class, args);
+	}
+
+}
---
+++ DataStructuresBackend/src/main/java/com/example/DataStructuresBackend/controller/DataStructureController.java
@@ -0,0 +1,10 @@
+package com.example.DataStructuresBackend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/DataStructure")
+public class DataStructureController {
+
+}
